38da6a632b730a862589f3015d59d135544fae30
Sadeesha94 <sadishanishara12@gmail.com>
2019-11-06 16:56:57 +0530

    first commit

---
+++ Evaluator/ExpressionEvaluator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Jace;
+
+namespace Evaluator
+{
+    public class ExpressionEvaluator
+    {
+        public double Evaluate(Dictionary<string, double> variables, string expression)
+        {
+            CalculationEngine engine = new CalculationEngine();
+            Func<Dictionary<string, double>, double> function = engine.Build(expression);
+            return function(variables); ;
+        }
+    }
+}
